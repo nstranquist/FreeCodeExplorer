7ce8b29c06f9b42bc3444e61fcc476ccbb3b8413
Nico Stranquist <nstranquist@gmail.com>
2020-02-04 15:44:48 -0600

    add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div>
-
+      <Button>Hello</Button>
     </div>
   );
 }
